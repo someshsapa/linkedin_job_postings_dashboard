5bca052792c574c548afa52ca2e0d628ad077ae0
somesh <someshsapa09@gmail.com>
2024-10-28 23:21:24 -0400

    added dataset

--- data_loader.py
+++ data_loader.py
@@ -4,4 +4,4 @@ import streamlit as st
 @st.cache_data
 def load_data():
     """Load job postings data from a CSV file."""
-    return pd.read_csv('data/postings.csv')
+    return pd.read_csv('data/postings_data.csv')
